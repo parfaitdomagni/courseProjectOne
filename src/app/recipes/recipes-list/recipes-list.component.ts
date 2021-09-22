de5bbade6335a08cfb313d73348d4db3ff84dd88
Parfait Domagni <parfaitdomagni@gmail.com>
2021-09-21 19:44:31 -0500

    second commit

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import{Recipe} from '../recipe.model';
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Tis is a simple text','https://www.bonappetit.com/recipe/slow-roast-gochujang-chicken'),
+    new Recipe('A Test Recipe', 'Tis is a simple text','https://www.bonappetit.com/recipe/slow-roast-gochujang-chicken')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
